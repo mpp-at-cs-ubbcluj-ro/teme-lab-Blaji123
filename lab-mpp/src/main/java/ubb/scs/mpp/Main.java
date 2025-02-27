e86be931ff0697c0a28ee4341771c8c70f1a9128
Blaji123 <david_blajan523@yahoo.com>
2025-02-27 14:37:48 +0200

    initial commit

---
+++ lab-mpp/src/main/java/ubb/scs/mpp/Main.java
@@ -0,0 +1,11 @@
+package ubb.scs.mpp;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+        Printer printer = new Printer();
+        printer.print("lab-mpp1");
+    }
+}
